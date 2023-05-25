4749b9341257521b705b2560833a06dcad8e104b
Alisson Guedes <alissonguedes87@gmail.com>
2023-05-25 17:58:01 -0300

    Atualizações iniciais - 25/05/2023 14h00

--- application/app/Http/Controllers/Admin/ConfigController.php
+++ application/app/Http/Controllers/Admin/ConfigController.php
@@ -1,75 +1,73 @@
 <?php
 
-namespace App\Http\Controllers\Admin{
+namespace App\Http\Controllers\Admin;
 
-	// use App\Models\Admin\IdiomaModel;
-	use App\Models\ConfigModel;
-	use App\Models\ModuloModel;
-	use Illuminate\Http\Request;
-	use Illuminate\Validation\Rule;
+// use App\Models\Admin\IdiomaModel;
+use App\Models\ConfigModel;
+use App\Models\ModuloModel;
+use Illuminate\Http\Request;
+use Illuminate\Validation\Rule;
 
-	class ConfigController extends Controller
+class ConfigController extends Controller
+{
+
+	public function __construct()
+	{
+		$this->config_model = new ConfigModel();
+		$this->modulo_model = new ModuloModel();
+		// $this->idioma_model = new IdiomaModel();
+	}
+
+	public function index(Request $request)
 	{
 
-		public function __construct()
-		{
-			$this->config_model = new ConfigModel();
-			$this->modulo_model = new ModuloModel();
-			// $this->idioma_model = new IdiomaModel();
+		if ($request->ajax()) {
+			$dados['paginate'] = $this->menu_model->getMenus()->paginate();
+			return view('admin.menus.list', $dados);
 		}
 
-		public function index(Request $request)
-		{
+		$dados['modulos'] = $this->modulo_model->getModulos();
 
-			if ($request->ajax()) {
-				$dados['paginate'] = $this->menu_model->getMenus()->paginate();
-				return view('admin.menus.list', $dados);
-			}
+		return view('admin.menus.index', $dados);
 
-			$dados['modulos'] = $this->modulo_model->getModulos();
+	}
 
-			return view('admin.menus.index', $dados);
+	public function show_form(Request $request, $id = null)
+	{
 
-		}
+		return view('admin.menus.edit');
+		// return redirect()->route('admin.menus')->send();
 
-		public function show_form(Request $request, $id = null)
-		{
+	}
 
-			return view('admin.menus.edit');
-			// return redirect()->route('admin.menus')->send();
+	public function create(Request $request)
+	{
 
-		}
+		$request->validate([
+			// 'titulo' => ['required', Rule::unique('tb_acl_menu', 'label')->ignore($request->id, 'id'), 'max:255'],
+			'titulo' => ['required', Rule::unique('tb_acl_menu', 'label'), 'max:255'],
+			'modulo' => ['required'],
+		]);
 
-		public function create(Request $request)
-		{
+		$id      = $this->menu_model->insertMenu($request);
+		$status  = 'success';
+		$message = 'As configurações foram salvas com sucesso!';
+		$url     = url()->route('admin.menus.edit', $id);
+		$type    = 'send';
 
-			$request->validate([
-				// 'titulo' => ['required', Rule::unique('tb_acl_menu', 'label')->ignore($request->id, 'id'), 'max:255'],
-				'titulo' => ['required', Rule::unique('tb_acl_menu', 'label'), 'max:255'],
-				'modulo' => ['required'],
-			]);
+		return json_encode(['status' => $status, 'message' => $message, 'type' => $type, 'url' => $url], 200);
 
-			$id      = $this->menu_model->insertMenu($request);
-			$status  = 'success';
-			$message = 'As configurações foram salvas com sucesso!';
-			$url     = url()->route('admin.menus.edit', $id);
-			$type    = 'send';
+	}
 
-			return json_encode(['status' => $status, 'message' => $message, 'type' => $type, 'url' => $url], 200);
+	public function patch(Request $request)
+	{
 
-		}
+		return $this->config_model->patch([$request->config => $request->value]);
 
-		public function patch(Request $request)
-		{
+	}
 
-			return $this->config_model->patch([$request->config => $request->value]);
-
-		}
-
-		public function delete(Request $request)
-		{
-
-		}
+	public function delete(Request $request)
+	{
 
 	}
 
