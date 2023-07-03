972fa7aced90766511ee76a2eaac5a35072a4a66
alisson guedes <alissonguedes@gmail.com>
2023-07-03 02:42:09 -0300

    Atualizações do site

--- application/resources/views/main/services/medicos.blade.php
+++ application/resources/views/main/services/medicos.blade.php
@@ -9,13 +9,9 @@
 
 	<div class="row">
 		<div class="col s12 mb-4">
-			<h5 class="mb-5">Telemedicina</h5>
-			{{-- <p>
-			Serviço médico através da plataforma.
-		</p>
-		<p>
-			Este serviço compõe atendimentos médicos para mais de 30 especialidades <strong>sem cobrança extra</strong>.
-		</p> --}}
+
+			<h4 class="mb-5">Telemedicina</h4>
+
 			<p>Com a Telemedicina, você pode ter acesso a consultas médicas virtuais com profissionais qualificados, sem precisar sair de casa. Nosso serviço foi projetado para oferecer atendimento médico de qualidade, de forma segura e acessível, através de videochamadas confidenciais.</p>
 
 			<p>Através da Telemedicina da Médicus24h, você pode agendar consultas com médicos especialistas, discutir seus sintomas, obter orientações médicas, receber diagnósticos e até mesmo ter prescrições emitidas, quando necessário. Tudo isso, no conforto e conveniência do seu lar.</p>
@@ -38,26 +34,129 @@
 
 	<div class="row">
 		<div class="col s12 mb-4">
-			<h5 class="mb-5">Exames a domicílio</h5>
-			<p>
-				Serviço de exames diversos que poderão ser realizados no domícilio do paciente.
-			</p>
-			<p>
-				Esse serviço pode ter uma coparticipação.
-			</p>
+
+			<h4 class="mb-5">Exames a domicílio</h4>
+
+			<p>Compreendemos a importância do cuidado com a sua saúde sem que isso implique em sacrificar seu tempo e conforto. Pensando nisso, oferecemos um serviço de exames a domicílio que traz a conveniência de realizá-los no conforto do seu lar.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Conforto e praticidade</strong>
+
+			<p>Com a correria do dia a dia, nem sempre é fácil encaixar uma visita a uma clínica ou laboratório. Com nossos exames a domicílio, você economiza tempo e evita deslocamentos, proporcionando maior comodidade à sua rotina.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Segurança e Higiene</strong>
+
+			<p>Entendemos a importância da segurança em tempos desafiadores. Nossos profissionais altamente treinados seguem rigorosos protocolos de higiene, garantindo a sua saúde e tranquilidade durante o processo de coleta de amostras.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Atendimento Personalizado</strong>
+
+			<p>Na Médicus24h, nosso objetivo é oferecer um atendimento personalizado e humano. Nossos profissionais se dedicam a entender suas necessidades específicas, tornando a experiência do exame mais acolhedora e empática.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Ampla Gama de Exames</strong>
+
+			<p>Oferecemos uma ampla variedade de exames que cobrem diversas especialidades médicas. Seja para check-ups regulares ou para investigações mais complexas, nossos serviços abrangem suas necessidades com eficiência e precisão.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Fácil agendamento</strong>
+
+			<p>Agendar seus exames a domicílio é simples. Basta entrar em contato com nossa equipe e escolher a melhor data e horário para a visita.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Coleta de Amostras</strong>
+
+			<p>Nossos profissionais chegarão à sua casa no horário agendado, devidamente equipados para realizar a coleta de amostras com total segurança e cuidado.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Resultados super rápidos</strong>
+
+			<p>Após a coleta, suas amostras serão enviadas para análise em nossos laboratórios de última geração. Os resultados serão disponibilizados em até 60 minutos, permitindo que você obtenha o diagnóstico de forma ágil e precisa diretamente em seu celular por Whatsapp ou através do nosso portal.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Sua Saúde em Primeiro Lugar!!!</strong>
+
+			<p>Na Médicus24h, priorizamos a sua saúde e bem-estar. Nossos exames a domicílio são projetados para oferecer comodidade, segurança e excelência nos cuidados médicos. Acreditamos que a prevenção é a melhor abordagem para uma vida saudável, e nossos serviços tornam esse processo mais acessível para você.</p>
+
+			{{-- <p>Entre em contato conosco hoje mesmo e agende seus exames a domicílio. Cuide de si mesmo e de quem você ama com a qualidade e confiança que só a Médicus24h pode oferecer. Estamos aqui para atendê-lo, trazendo a saúde até a sua porta!</p> --}}
+
 		</div>
 	</div>
 
 
 	<div class="row">
 		<div class="col s12 mb-4">
+
 			<h5 class="mb-5">Outros serviços</h5>
-			<ul class="list-style-dotted">
+
+			{{-- <ul class="list-style-dotted">
 				<li>Venda e aluguéis de produtos</li>
 				<li>Contratação de serviços médicos</li>
 				<li>Cursos e treinamentos</li>
 				<li>Doula</li>
-			</ul>
+			</ul> --}}
+
+			{{-- Bem-vindo à Médicus24h: Seu Parceiro em Saúde Integral!
+
+			Na Médicus24h, estamos comprometidos em oferecer soluções completas para atender às suas necessidades médicas. Além de nossos serviços de excelência em atendimento domiciliar, oferecemos uma ampla gama de recursos adicionais para complementar sua jornada de cuidados de saúde.
+
+			Venda e Aluguéis de Produtos Médicos de Qualidade
+
+			Entendemos que, em certos momentos, pode ser necessário ter acesso a produtos médicos específicos para promover sua saúde e bem-estar contínuos. Em nossa loja online, você encontrará uma seleção abrangente de produtos médicos de qualidade disponíveis para venda ou aluguel. Nossos produtos são cuidadosamente selecionados e atendem aos mais altos padrões de qualidade, garantindo a sua segurança e satisfação.
+
+			Contratação de Serviços Médicos Profissionais
+
+			Na Médicus24h, estamos comprometidos em fornecer uma ampla gama de serviços médicos profissionais para atender às suas necessidades específicas. Nossa equipe é composta por médicos especialistas em diversas áreas, prontos para oferecer atendimento médico de qualidade e orientação adequada. Oferecemos serviços de consultas médicas, segundas opiniões, acompanhamento de casos e muito mais, proporcionando cuidados abrangentes e personalizados para sua saúde.
+
+			Cursos e Treinamentos para Profissionais de Saúde
+
+			Acreditamos na importância do aprendizado contínuo e no desenvolvimento profissional. É por isso que oferecemos cursos e treinamentos para profissionais de saúde em diversas áreas. Nossos programas de capacitação abrangem tópicos relevantes e atuais, ministrados por especialistas renomados. Seja você um médico, enfermeiro, técnico ou profissional de saúde, nossos cursos e treinamentos são projetados para aprimorar suas habilidades e conhecimentos, capacitando-o a oferecer o melhor atendimento aos seus pacientes.
+
+			Serviços de Doula: Apoio e Empoderamento durante a Maternidade
+
+			Entendemos que a experiência da maternidade é única e desafiadora. É por isso que oferecemos serviços de doula para apoiar e empoderar as mulheres durante a jornada da maternidade. Nossas doulas altamente qualificadas estão aqui para fornecer apoio emocional, educacional e físico antes, durante e após o parto. Elas são treinadas para oferecer conforto, informações e suporte individualizado, garantindo uma experiência mais tranquila e positiva para as mães e suas famílias.
+
+			Na Médicus24h, estamos empenhados em fornecer soluções abrangentes para atender às suas necessidades de saúde. Seja através da venda de produtos médicos, contratação de serviços médicos profissionais, cursos e treinamentos ou serviços de doula, estamos aqui para ser seu parceiro de confiança em sua jornada para uma vida saudável e feliz.
+
+			Entre em contato conosco hoje mesmo e descubra como a Médicus24h pode ajudá-lo a alcançar sua melhor saúde e bem-estar! --}}
+
+			<p>Na Médicus24h, nosso compromisso é fornecer um conjunto abrangente de serviços e soluções para atender às suas necessidades de saúde. Além de todos os serviços já mencionados, conheça mais outros serviços que ainda podemos disponibilizar.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">1. Venda e Aluguéis de Produtos Médicos:</strong>
+
+			<p>Oferecemos uma ampla variedade de produtos médicos para venda e aluguel, proporcionando o suporte necessário para o cuidado com a sua saúde em casa. Desde equipamentos de monitoramento de saúde até dispositivos de assistência, nossas opções de produtos garantem a sua comodidade e bem-estar.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">2. Contratação de Serviços Médicos:</strong>
+
+			<p>Nossa plataforma facilita a contratação de serviços médicos altamente qualificados. Com um time de profissionais experientes e dedicados, oferecemos atendimentos domiciliares, consultas médicas e serviços especializados para garantir a sua saúde e tranquilidade.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">3. Cursos e Treinamentos:</strong>
+
+			<p>Acreditamos que a educação é a base para uma vida saudável. Com nossos cursos e treinamentos, você terá acesso a conteúdos enriquecedores, ministrados por profissionais renomados, que visam promover o conhecimento e a conscientização sobre saúde, prevenção de doenças e cuidados específicos.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">4. Doula:</strong>
+
+			<p>Acompanhamento durante a gestação e parto é uma etapa crucial da jornada de uma mulher. Nossas doulas experientes e atenciosas oferecem suporte emocional, físico e informativo para tornar a experiência do parto mais positiva e empoderadora.</p>
+
+			<h5 class="bold red-text text-darken-2 mt-3 mb-3">Por que escolher a Médicus24h?</h5>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Excelência no Atendimento:</strong>
+
+			<p>Nossa equipe é composta por profissionais qualificados, comprometidos em fornecer um atendimento personalizado e de qualidade.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Comodidade e Segurança</strong>
+
+			<p>Nossos serviços são pensados para atender suas necessidades no conforto do seu lar, garantindo a sua segurança e bem-estar.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Variedade de Opções</strong>
+
+			<p>Temos uma gama completa de soluções médicas, produtos e serviços, proporcionando uma abordagem holística para cuidar da sua saúde.</p>
+
+			<strong class="bold text-darken-2 mt-3 mb-3">Confiabilidade e Transparência</strong>
+
+			<p>Na Médicus24h, a confiança é essencial. Nossos serviços são realizados com transparência, respeito e ética, visando sempre o melhor para você.</p>
+
+			<h5 class="bold red-text text-darken-2 mt-3 mb-3">Cuide de sua Saúde com a Médicus24h</h5>
+
+			<p>Escolher a Médicus24h significa investir na sua saúde e bem-estar. Estamos comprometidos em fornecer soluções integradas e eficazes para todas as suas necessidades médicas. Seja através da venda e aluguel de produtos médicos, contratação de serviços médicos, cursos e treinamentos ou acompanhamento com nossas doulas, você encontrará na Médicus24h uma equipe dedicada a cuidar de você e sua família.</p>
+
+			<p>Contate-nos hoje mesmo e descubra como a Médicus24h pode fazer a diferença na sua vida.</p>
+
+			<p>Sua saúde é nossa prioridade!</p>
+
 		</div>
 	</div>
 @endsection
